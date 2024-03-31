380f3f11dd6ecbe885b520b1035f4cb3182b1dc3
rafaelborja <rafaelborja@gmail.com>
2024-03-30 20:50:02 -0400

    Fixes due to permissions and clipboard related issues.

--- background.js
+++ background.js
@@ -1,24 +1,41 @@
-
 chrome.action.onClicked.addListener(() => {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
+    chrome.tabs.query({currentWindow: true}, function (tabs) {
         let fullContent = "";
         let count = 0;
-
         tabs.forEach((tab) => {
             chrome.scripting.executeScript({
                 target: {tabId: tab.id},
                 function: tabContent,
             }, (results) => {
                 count++;
-                if(results[0]) {
+                if (results[0]) {
                     const {title, url, content, timestamp} = results[0].result;
                     fullContent += `Title: ${title}\nTime: ${timestamp}\nURL: ${url}\n\n${content}\n\n---\n\n`;
+                    console.log(fullContent)
                 }
-                if(count === tabs.length) {
-                    navigator.clipboard.writeText(fullContent).then(() => {
-                        console.log('All tab contents copied to clipboard!');
-                    }, (err) => {
-                        console.error('Error copying tab contents: ', err);
+                if (count === tabs.length) {
+                    chrome.tabs.update(tab.id, {active: true});
+                    console.log(tab.id)
+                    console.log(fullContent)
+
+                    chrome.scripting.executeScript({
+                        args: [fullContent],
+                        target: {tabId: tab.id},
+                        func: function (fullContent) {
+                            // write your code here
+                            console.log("This is " + fullContent);
+                            console.log(' Start contents copied to clipboard!');
+
+                            navigator.clipboard.writeText(fullContent).then(() => {
+                                console.log('All tab contents copied to clipboard!');
+                            }, (err) => {
+                                console.error('Error copying tab contents: ', err);
+                            });
+
+                            window.focus(); // Attempt to focus the window
+                            document.body.focus(); // Attempt to focus the document
+
+                        },
                     });
                 }
             });
